f971d1ddcdda81c4c2a4c94298243c7d4bcfb0c2
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2014-11-19 23:37:25 +0100

    surprising impl :-)

--- src/main/java/net/kaczmarzyk/kata/RomanNumeralConverter.java
+++ src/main/java/net/kaczmarzyk/kata/RomanNumeralConverter.java
@@ -30,11 +30,17 @@ public class RomanNumeralConverter {
 
 	private List<RomanSymbol> symbols = Arrays.asList(
 		new RomanSymbol(1000, "M"),
+		new RomanSymbol(900, "CM"),
 		new RomanSymbol(500, "D"),
+		new RomanSymbol(400, "CD"),
 		new RomanSymbol(100, "C"),
+		new RomanSymbol(90, "XC"),
 		new RomanSymbol(50, "L"),
+		new RomanSymbol(40, "XL"),
 		new RomanSymbol(10, "X"),
+		new RomanSymbol(9, "IX"),
 		new RomanSymbol(5, "V"),
+		new RomanSymbol(4, "IV"),
 		new RomanSymbol(1, "I")
 	);
 	
